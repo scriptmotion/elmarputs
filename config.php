f2c6bd69d5c92e193dedb8746c164d218926a578
Paul du Long <info@scriptmotion.nl>
2013-07-15 16:03:49 +0200

    teststtt

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-//Paul du Long
+//Paul du Long - cool
 
 /*
 |--------------------------------------------------------------------------
